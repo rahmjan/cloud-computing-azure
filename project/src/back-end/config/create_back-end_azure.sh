bba4633a54c05f46e0fb150a1d3b8546f4226f56
jr <rahmjan@gmail.com>
2018-11-19 02:15:51 +0100

    Scaling

--- project/src/back-end/config/create_back-end_azure.sh
+++ project/src/back-end/config/create_back-end_azure.sh
@@ -1,73 +1,64 @@
+#!/usr/bin/env bash
+
 USER_NAME=myAdmin
 ADMIN_PASS=Admin1234
-GITHUB_ACC=rahmjan  ## you need to change it
-
-### Create azure back-end ###
+ADDRESS=francecentral.cloudapp.azure.com
+SSH_KEY=./ssh/rsa_key
+GITHUB_ACC=rahmjan
 NUM_OF_NODES=2
 
 # Please login
-az login
-
-# Resource group
-az group create --name lingi2145.fr --location francecentral
-
-# Create VM
-for i in `seq 1 ${NUM_OF_NODES}`
-do
-    az group deployment create --resource-group lingi2145.fr \
-                               --template-file ./azure-docker-vm.json \
-                               --parameters "{\"adminUsername\": {\"value\": \"${USER_NAME}\"},
-                                              \"adminPassword\": {\"value\": \"${ADMIN_PASS}\"},
-                                              \"gitHubAccount\": {\"value\": \"${GITHUB_ACC}\"},
-                                              \"dnsNameForPublicIP\": {\"value\": \"node${i}\"},
-                                              \"sshKeyData\": {\"value\": \"$(cat ./ssh/rsa_key.pub)\"}}"
-done
+#az login
+#
+## Resource group
+#az group create --name lingi2145.fr --location francecentral
+#
+## Create VM
+#for i in `seq 1 ${NUM_OF_NODES}`
+#do
+#    az group deployment create --resource-group lingi2145.fr \
+#                               --template-file ./azure-docker-vm.json \
+#                               --parameters "{\"adminUsername\": {\"value\": \"${USER_NAME}\"},
+#                                              \"adminPassword\": {\"value\": \"${ADMIN_PASS}\"},
+#                                              \"gitHubAccount\": {\"value\": \"${GITHUB_ACC}\"},
+#                                              \"dnsNameForPublicIP\": {\"value\": \"node${i}\"},
+#                                              \"sshKeyData\": {\"value\": \"$(cat ./ssh/rsa_key.pub)\"}}"
+#done
 
 # Add key to know_hosts
 for i in `seq 1 ${NUM_OF_NODES}`
 do
-    ssh-keyscan -Ht rsa node${i}-${GITHUB_ACC}.francecentral.cloudapp.azure.com >> ~/.ssh/known_hosts
+    ssh-keyscan -Ht rsa node${i}-${GITHUB_ACC}.${ADDRESS} >> ~/.ssh/known_hosts
 done
 
 # Get IP from leader node
-leader_ip=$(dig +short node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com)
+leader_ip=$(dig +short node1-${GITHUB_ACC}.${ADDRESS})
 
 # Init Swarm node
 echo "### Initializing Swarm node ..."
-ssh -i ./ssh/rsa_key ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com "docker swarm init --advertise-addr ${leader_ip}"
+ssh -i ${SSH_KEY} ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS} "docker swarm init --advertise-addr ${leader_ip}"
 
 # Swarm token
-token=$(ssh -i ./ssh/rsa_key ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com "docker swarm join-token worker -q")
+token=$(ssh -i ${SSH_KEY} ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS} "docker swarm join-token worker -q")
 
 # Joining nodes
 echo "### Joining nodes ..."
 for i in `seq 2 ${NUM_OF_NODES}`
 do
-    MY_IP=$(dig +short node${i}-${GITHUB_ACC}.francecentral.cloudapp.azure.com)
-    ssh -i ./ssh/rsa_key ${USER_NAME}@node${i}-${GITHUB_ACC}.francecentral.cloudapp.azure.com "docker swarm join --advertise-addr ${MY_IP} --token ${token} ${leader_ip}:2377"
-done
-
-# Set daemon type
-for i in `seq 2 ${NUM_OF_NODES}`
-do
-    ssh -i ./ssh/rsa_key ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com "docker node update --label-add type=daemon node${i}"
+    MY_IP=$(dig +short node${i}-${GITHUB_ACC}.${ADDRESS})
+    ssh -i ${SSH_KEY} ${USER_NAME}@node${i}-${GITHUB_ACC}.${ADDRESS} "docker swarm join --advertise-addr ${MY_IP} --token ${token} ${leader_ip}:2377"
 done
 
 # Copy data to manager
 echo "### Copy data to manager ..."
-rsync -e "ssh -i ./ssh/rsa_key" -r ./swarm_config.yml ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com:./swarm_config.yml
-rsync -e "ssh -i ./ssh/rsa_key" -r ./elastic_scaling.sh ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com:./elastic_scaling.sh
+rsync -e "ssh -i ${SSH_KEY}" -r ./swarm_config.yml ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS}:./swarm_config.yml
+rsync -e "ssh -i ${SSH_KEY}" -r ./ssh/ ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS}:./ssh/
+rsync -e "ssh -i ${SSH_KEY}" -r ./elastic_scaling_azure.sh ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS}:./elastic_scaling_azure.sh
 
 # Deploying services
 echo "### Deploying services ..."
-ssh -i ./ssh/rsa_key ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com "docker stack deploy -c ./swarm_config.yml mySwarm"
-ssh -i ./ssh/rsa_key ${USER_NAME}@node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com "./elastic_scaling.sh &"
+ssh -i ${SSH_KEY} ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS} "docker stack deploy -c ./swarm_config.yml mySwarm"
+ssh -i ${SSH_KEY} ${USER_NAME}@node1-${GITHUB_ACC}.${ADDRESS} "./elastic_scaling_azure.sh > output_elastic &" &> /dev/null
 
 # End
-echo "### The swarm leader ip is: ${leader_ip} and address: node1-${GITHUB_ACC}.francecentral.cloudapp.azure.com"
-
-
-# node1-rahmjan.francecentral.cloudapp.azure.com
-# ssh -i ./ssh/rsa_key myAdmin@node1-rahmjan.francecentral.cloudapp.azure.com
-#rsync -e "ssh -i ./ssh/rsa_key" -r ./../../../project/src/back-end/ myAdmin@node1-rahmjan.francecentral.cloudapp.azure.com:./back-end/
-#ssh -i ./ssh/rsa_key myAdmin@node1-rahmjan.francecentral.cloudapp.azure.com "docker stack deploy -c ./back-end/swarm_config.yml mySwarm"
+echo "### The swarm leader ip is: ${leader_ip} and address: node1-${GITHUB_ACC}.${ADDRESS}"
---
+++ project/src/back-end/config/elastic_scaling_azure.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -
+
+USER_NAME=myAdmin
+ADDRESS=francecentral.cloudapp.azure.com
+SSH_KEY=./ssh/rsa_key
+GITHUB_ACC=rahmjan
+NUM_OF_NODES=2
+
+["ns1"]="fd00:300:3:1000::1"
+
+SERVICES=(users-daemon users-db catalog catalog-db \
+          shopping_cart shopping_cart-db logging logging-db \
+          purchase purchase-db)
+
+# Add key to know_hosts
+for i in `seq 1 ${NUM_OF_NODES}`
+do
+    ssh-keyscan -Ht rsa node${i}-${GITHUB_ACC}.${ADDRESS} >> ~/.ssh/known_hosts
+done
+
+# Check CPU
+while true
+do
+    for service in "${!SERVICES[@]}"
+    do
+        cpu_usage=0
+        scale_to=0
+
+        for i in `seq 1 ${NUM_OF_NODES}`
+        do
+            value=$(ssh -i ${SSH_KEY} ${USER_NAME}@node${i}-${GITHUB_ACC}.${ADDRESS} "docker stats --no-stream | grep ${service} | awk '{sum += \$3} END {print sum}'")
+            cpu_usage=$(echo "${cpu_usage} ${value}" | awk '{print $1 + $2}')
+        done
+
+        cpu_usage=$(echo "${cpu_usage} ${NUM_OF_NODES}" | awk '{print $1 / $2}')
+
+        # Big Switch
+        if (( $(echo "5 > $cpu_usage" | bc -l) ))
+        then
+            scale_to=1
+        elif (( $(echo "10 > $cpu_usage" | bc -l) ))
+        then
+            scale_to=3
+        elif (( $(echo "20 > $cpu_usage" | bc -l) ))
+        then
+            scale_to=6
+        else
+            scale_to=9
+        fi
+
+        echo "HERE: ${SERVICES[service]}"
+        if (( "${SERVICES[$service]}" != "${scale_to}" ))
+        then
+            echo "${service}: ${cpu_usage}% cpu, scale to number of services: ${scale_to}"
+            docker service scale mySwarm_${service}=${scale_to} 1> /dev/null
+        fi
+    done
+done
