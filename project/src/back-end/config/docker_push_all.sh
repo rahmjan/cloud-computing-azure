39a596d1a13c9b5f3d66142aceff0372da35b68f
jr <rahmjan@gmail.com>
2018-12-08 18:49:44 +0100

    docker push script

---
+++ project/src/back-end/config/docker_push_all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker push rahmjan/users-db && \
+docker push rahmjan/users-daemon && \
+docker push rahmjan/catalog && \
+docker push rahmjan/catalog-db && \
+docker push rahmjan/shopping_cart && \
+docker push rahmjan/shopping_cart-db && \
+docker push rahmjan/purchase && \
+docker push rahmjan/purchase-db && \
+docker push rahmjan/logging && \
+docker push rahmjan/logging-db \
+docker push rahmjan/recomm && \
+docker push rahmjan/recomm-db
